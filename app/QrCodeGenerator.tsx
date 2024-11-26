66aa014c410dc808e91eb148e49715ef5e163af3
Mduduzi-Ndlovu1 <129621544+Mduduzi-Ndlovu1@users.noreply.github.com>
2024-11-26 19:26:06 +0200

    first commit

---
+++ app/QrCodeGenerator.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function QrCodeGenerator() {
+  return (
+    <div className='relative mx-6 flex max-w-[1250px] w-full min-h-[700px] h-full'>
+        <div className='flex-1 flex flex-col w-full h-auto mx-auto bg-[#ecf7ff]/80 backdrop-blur-md shadow-sm border-2 border-white/40 rounded-xl'>
+
+        </div>
+    </div>
+  )
+}
+
+export default QrCodeGenerator
