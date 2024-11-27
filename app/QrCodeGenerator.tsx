885dfed0249778f2bd8b0eb36c1311acd3931b34
Mduduzi-Ndlovu1 <129621544+Mduduzi-Ndlovu1@users.noreply.github.com>
2024-11-27 21:08:53 +0200

    feat: Add QR Code Generator with URL and Email options
    - Implemented a QR Code generator allowing users to input a URL or email details (email, subject, message).
    - Users can customize QR code colors, background, and upload a logo.
    - Added file input for logo customization, with image preview and integration in the QR code.
    - Integrated functionality to generate and download QR codes as PNG images.
    - Styled the components for a clean and responsive design with Tabs for different QR code types (link/email).

--- app/QrCodeGenerator.tsx
+++ app/QrCodeGenerator.tsx
@@ -13,26 +13,27 @@ import {
   TabsList,
   TabsTrigger,
 } from "@/components/ui/tabs"
-import { LayoutGrid, Link, Mail } from 'lucide-react';
+import { Download, LayoutGrid, Link, Mail } from 'lucide-react';
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
 import { QRCodeSVG } from 'qrcode.react';
+import { Textarea } from '@/components/ui/textarea';
+import { Button } from '@/components/ui/button';
 
 function QrCodeGenerator() {
-
   const [url, setUrl] = useState("");
   const [color, setColor] = useState("#ffffff");
   const [bgColor, setBgColor] = useState("#057fff");
   const [logo, setLogo] = useState<string | null>(null);
   const [logoFile, setLogoFile] = useState<File | null>(null);
   const [qrType, setQrType] = useState("link");
-  const [email, setEmail] = useState("")
-  const [message, setMessage] = useState("")
-
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [subject, setSubject] = useState("");
 
   return (
     <div className='relative mx-6 flex max-w-[1250px] w-full min-h-[700px] h-full'>
-        <Card className='flex-1 flex flex-col w-full h-auto mx-auto bg-[#ecf7ff]/80 backdrop-blur-md shadow-sm border-2 border-white/40 rounded-xl'>
+      <Card className='flex-1 flex flex-col w-full h-auto mx-auto bg-[#ecf7ff]/80 backdrop-blur-md shadow-sm border-2 border-white/40 rounded-xl'>
         <CardHeader>
           <CardTitle className="text-3xl font-bold text-center text-[#037fff]">
             QR Code Generator
@@ -41,45 +42,100 @@ function QrCodeGenerator() {
         <CardContent className='flex-1'>
           <div className='h-full flex flex-col md:flex-row gap-8'>
             <div className='flex-1 space-y-6'>
-              <Tabs className='space-y-6' 
+              <Tabs className='space-y-6'
                 defaultValue='link'
                 onValueChange={(val) => setQrType(val)}
-                >
-                  <TabsList className='h-10 w-full grid grid-cols-2 bg-[#057fff] text-lg'>
-                    <TabsTrigger value='link' className="text-white font-bold">
-                      <Link className='w-4 h-4 mr-2'/>
-                      Link
-                    </TabsTrigger>
-                    <TabsTrigger value='email' className='text-white font-bold'>
-                      <Mail className='w-4 h-4 mr-2'/>
-                      Email
-                    </TabsTrigger>
-                  </TabsList>
-                  <TabsContent value='link'>
-                    <div className='space-y-6'>
-                      <div className='space-y-2'>
-                        <Label htmlFor='url' className='font-semibold text-[#057fff]'>
-                          URL
-                        </Label>
-                        {/* 38:23 */}
-                        <Input 
-                          id='url'
-                          type='text'
-                          value={url}
-                          placeholder='https://example.com'
-                          onChange={(e) => setUrl(e.target.value)}
-                          className='w-full border-2 border-white/70 focus:border-[#057fff]/70 rounded-md outline-none focus-visible:ring-0 placeholder:text-gray-400'
-                        />
-                      </div>
+              >
+                <TabsList className='h-10 w-full grid grid-cols-2 bg-[#057fff] text-lg'>
+                  <TabsTrigger value='link' className="text-white font-bold">
+                    <Link className='w-4 h-4 mr-2' />
+                    Link
+                  </TabsTrigger>
+                  <TabsTrigger value='email' className='text-white font-bold'>
+                    <Mail className='w-4 h-4 mr-2' />
+                    Email
+                  </TabsTrigger>
+                </TabsList>
+
+                {/* Tab content for link section */}
+                <TabsContent value='link'>
+                  <div className='space-y-6'>
+                    <div className='space-y-2'>
+                      <Label htmlFor='url' className='font-semibold text-[#057fff]'>
+                        URL
+                      </Label>
+                      <Input
+                        id='url'
+                        type='text'
+                        value={url}
+                        placeholder='https://example.com'
+                        onChange={(e) => setUrl(e.target.value)}
+                        className='w-full border-2 border-white/70 focus:border-[#057fff]/70 rounded-md outline-none focus-visible:ring-0 placeholder:text-gray-400'
+                      />
                     </div>
-                  </TabsContent>
+                  </div>
+                </TabsContent>
+
+                {/* Tab content for Email Section */}
+                <TabsContent value='email'>
+                  <div className='space-y-4'>
+                    <div className='space-y-2'>
+                      <Label
+                        htmlFor='email'
+                        className='font-semibold text-[#057fff]'
+                      >
+                        Email
+                      </Label>
+                      <Input
+                        id='email'
+                        type='email'
+                        value={email}
+                        placeholder='Enter Email'
+                        onChange={(e) => setEmail(e.target.value)}
+                        className='w-full border-2 border-white/70 focus:border-[#057fff]/70 rounded-md outline-none focus-visible:ring-0 placeholder:text-gray-400'
+                      />
+                    </div>
+                    <div className='space-y-2'>
+                      <Label
+                        htmlFor='subject'
+                        className='font-semibold text-[#057fff]'
+                      >
+                        Subject
+                      </Label>
+                      <Input
+                        id='subject'
+                        type='text'
+                        value={subject}
+                        placeholder='Enter the Email Subject'
+                        onChange={(e) => setSubject(e.target.value)}
+                        className='w-full border-2 border-white/70 focus:border-[#057fff]/70 rounded-md outline-none focus-visible:ring-0 placeholder:text-gray-400'
+                      />
+                    </div>
+                    <div className='space-y-2'>
+                      <Label
+                        htmlFor='message'
+                        className='font-semibold text-[#057fff]'
+                      >
+                        Message
+                      </Label>
+                      <Textarea
+                        id='message'
+                        value={message}
+                        placeholder='Enter the Email Message'
+                        onChange={(e) => setMessage(e.target.value)}
+                        className='w-full border-2 border-white/70 focus:border-[#057fff]/70 rounded-md outline-none focus-visible:ring-0 placeholder:text-gray-400'
+                      />
+                    </div>
+                    <Button className='py-7 px-8 bg-[#057fff] font-bold rounded-full uppercase'>
+                      Generate Email QR Code
+                    </Button>
+                  </div>
+                </TabsContent>
               </Tabs>
               <div className='space-y-4'>
-                {/* color selector section */}
                 <div className='flex space-x-4'>
-                  {/* QR COlor section */}
                   <div className='space-y-2 flex-1'>
-                    <Label 
+                    <Label
                       htmlFor='color'
                       className='font-semibold text-[#057fff]'
                     >
@@ -87,9 +143,9 @@ function QrCodeGenerator() {
                     </Label>
                     <div className='flex items-center gap-1'>
                       <div className='relative w-12 flex-1 h-12 rounded-md border-2 border-white/70'
-                      style={{backgroundColor: color}}
+                        style={{ backgroundColor: color }}
                       >
-                        <Input 
+                        <Input
                           type='color'
                           id='color'
                           value={color}
@@ -105,10 +161,8 @@ function QrCodeGenerator() {
                       />
                     </div>
                   </div>
-
-                  {/* Background Color section */}
                   <div className='space-y-2 flex-1'>
-                    <Label 
+                    <Label
                       htmlFor='color'
                       className='font-semibold text-[#057fff]'
                     >
@@ -116,9 +170,9 @@ function QrCodeGenerator() {
                     </Label>
                     <div className='flex items-center gap-1'>
                       <div className='relative w-12 flex-1 h-12 rounded-md border-2 border-white/70'
-                      style={{backgroundColor: bgColor}}
+                        style={{ backgroundColor: bgColor }}
                       >
-                        <Input 
+                        <Input
                           type='color'
                           id='color'
                           value={bgColor}
@@ -134,12 +188,7 @@ function QrCodeGenerator() {
                       />
                     </div>
                   </div>
-
-                  
-
                 </div>
-
-                {/* select logo section */}
                 <div className='space-y-2'>
                   <Label
                     htmlFor='logo'
@@ -147,22 +196,18 @@ function QrCodeGenerator() {
                   >
                     Logo
                   </Label>
-                  {/* logo input section */}
                   <Input
                     type='file'
                     id='logo'
                     accept='image/*'
                     onChange={(e: any) => {
-                      if(e.target.files && e.target.files[0]){
+                      if (e.target.files && e.target.files[0]) {
                         setLogoFile(e.target.files[0]);
-
                         const reader = new FileReader();
-
                         reader.onloadend = () => {
                           setLogo(reader.result as string);
                         }
                         reader.readAsDataURL(e.target.files[0]);
-
                       }
                     }}
                     className='w-full border-2 bg-transparent border-white/70 focus:border-[#057fff]/70 rounded-md outline-none focus-visible:ring-0 placeholder:text-gray-400'
@@ -170,34 +215,48 @@ function QrCodeGenerator() {
                 </div>
               </div>
             </div>
-            <div className='relative flex-1 bg-[#037fff] rounded-lg flex -flex-col justify-center space-y-6 '>
+            <div className='relative flex-1 bg-[#037fff] rounded-lg flex flex-col justify-center items-center space-y-6'>
               <span>
-                <LayoutGrid className='w-12 h-12 text-white absolute top-4 right-4'/>
+                <LayoutGrid className='w-12 h-12 text-white absolute top-4 right-4' />
               </span>
-
               <div id='qr-code' className='flex justify-center items-center'>
-                <div className='relative '>
-                  <QRCodeSVG 
+                <div className='relative'>
+                  <QRCodeSVG
                     value={url}
                     size={256}
                     fgColor={bgColor}
                     bgColor={color}
-                    imageSettings={logo ? {
-                      src: logo,
-                      height:50,
-                      width: 50,
-                      excavate: true
-                    }: undefined}
+                    imageSettings={
+                      logo
+                        ? {
+                            src: logo,
+                            height: 50,
+                            width: 50,
+                            excavate: true,
+                          }
+                        : undefined
+                    }
                   />
+                  {logo && (
+                    <img
+                      src={logo}
+                      className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-12 h-12'
+                    />
+                  )}
                 </div>
+              </div>
+              <div className='flex justify-center items-center space-x-4'>
+                <Button variant='outline'>
+                  <Download  className='w-4 h-4 mr-2' />
+                  <span>Download PNG</span>
+                </Button>
               </div>
             </div>
           </div>
         </CardContent>
-
-        </Card>
+      </Card>
     </div>
-  )
+  );
 }
 
-export default QrCodeGenerator
+export default QrCodeGenerator;
